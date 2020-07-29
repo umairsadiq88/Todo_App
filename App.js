410823845b8a18161b60f43ff7a560f5c2d2def3
umairsadiq88 <umairsadiq2010@gmail.com>
2020-07-29 21:14:54 +0500

    update

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ function addTodo() {
     //create delete button
     var delBtn = document.createElement("button")
     var delText = document.createTextNode("Delete")
-    delBtn.setAttribute("class", "btn")
+    delBtn.setAttribute("class", "DelBtn")
     delBtn.setAttribute("onclick", "deleteItem(this)")
     delBtn.appendChild(delText)
    
@@ -23,10 +23,8 @@ function addTodo() {
     editBtn.setAttribute("class", "editBtn")
     editBtn.setAttribute("onclick", "editItem(this)") 
 
-
-
-    li.appendChild(delBtn)
     li.appendChild(editBtn)
+    li.appendChild(delBtn)
 
     list.appendChild(li)
 
@@ -50,121 +48,3 @@ function deleteItem(e){
 function deleteAll(){
     list.innerHTML = ""
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const container = document.querySelector('.container');
-// var inputValue = document.querySelector('.input');
-// const add = document.querySelector('.add');
-
-// if(window.localStorage.getItem("todos") == undefined){
-//      var todos = [];
-//      window.localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-// var todosEX = window.localStorage.getItem("todos");
-// var todos = JSON.parse(todosEX);
-
-
-// class item{
-// 	constructor(name){
-// 		this.createItem(name);
-// 	}
-//     createItem(name){
-//     	var itemBox = document.createElement('div');
-//         itemBox.classList.add('item');
-
-//     	var input = document.createElement('input');
-//     	input.type = "text";
-//     	input.disabled = true;
-//     	input.value = name;
-//     	input.classList.add('item_input');
-
-//     	var edit = document.createElement('button');
-//     	edit.classList.add('edit');
-//     	edit.innerHTML = "EDIT";
-//     	edit.addEventListener('click', () => this.edit(input, name));
-
-//     	var remove = document.createElement('button');
-//     	remove.classList.add('remove');
-//     	remove.innerHTML = "REMOVE";
-//     	remove.addEventListener('click', () => this.remove(itemBox, name));
-
-//     	container.appendChild(itemBox);
-
-//         itemBox.appendChild(input);
-//         itemBox.appendChild(edit);
-//         itemBox.appendChild(remove);
-
-//     }
-
-//     edit(input, name){
-//         if(input.disabled == true){
-//            input.disabled = !input.disabled;
-//         }
-//     	else{
-//             input.disabled = !input.disabled;
-//             let indexof = todos.indexOf(name);
-//             todos[indexof] = input.value;
-//             window.localStorage.setItem("todos", JSON.stringify(todos));
-//         }
-//     }
-
-//     remove(itemBox, name){
-//         itemBox.parentNode.removeChild(itemBox);
-//         let index = todos.indexOf(name);
-//         todos.splice(index, 1);
-//         window.localStorage.setItem("todos", JSON.stringify(todos));
-//     }
-// }
-
-// add.addEventListener('click', check);
-// window.addEventListener('keydown', (e) => {
-// 	if(e.which == 13){
-// 		check();
-// 	}
-// })
-
-// function check(){
-// 	if(inputValue.value != ""){
-// 		new item(inputValue.value);
-//         todos.push(inputValue.value);
-//         window.localStorage.setItem("todos", JSON.stringify(todos));
-// 		inputValue.value = "";
-// 	}
-// }
-
-
-// for (var v = 0 ; v < todos.length ; v++){
-//     new item(todos[v]);
-// }
-
-
-// new item("sport");
